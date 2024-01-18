c88d0546f97dc0cc4297732b4e72e4275909fa2a
Sumitrjk9752 <156745082+Sumitrjk9752@users.noreply.github.com>
2024-01-18 08:46:23 +0000

    string

---
+++ datatypre.js
@@ -0,0 +1,17 @@
+// datatype in js 
+Number 
+BigInt
+String     //userd in double quotes and String quotes and back ticks
+Boolean
+null       //standalone value not 0 and dtatype is object 
+undefined  // when not assigned any value 
+Symbol
+
+// special data type 
+
+Object 
+let bollvalue = "33abc"
+let convertNum = Number(bollvalue)
+ 
+console.log(convertNum);
+console.log(typeof convertNum);
--- practice.js
+++ practice.js
@@ -1,4 +1,18 @@
-var x = 20;
-console.log(x);
-var y = 10;
-console.log(x+y);
+// var x = 20;
+// console.log(x);
+// var y = 10;
+// console.log(x+y);
+
+let a = "15";
+let b = "35";
+const fname = "avinash"
+var city = "jabalpur"
+email = "avinash@gail.com"
+
+console.log([a,b,fname,city,email]);
+
+// do not use VAR as the system do not understrand block Scope. also do not define a varible without let and const.
+
+// const variables can not be charged
+const useame = "anurag"
+console.log(useame);
